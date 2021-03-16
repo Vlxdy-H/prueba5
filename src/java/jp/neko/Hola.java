17fce5dfa58a462063270dcccc46beea8d70457a
Vlxdy-H <No-reply-es@outlook.es>
2021-03-16 10:46:33 -0400

    commit

---
+++ src/java/jp/neko/Hola.java
@@ -0,0 +1,43 @@
+package jp.neko;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "Hola", urlPatterns = {"/Hola"})
+public class Hola extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Hola</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>HOLA MUNDO</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
